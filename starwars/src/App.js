cd9e2dea2f1b70a0231dbce794561bd1c798d925
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-01-12 18:35:53 -0800

    Added styled components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import CharacterList from "./components/CharacterList";
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  color: #443e3e;
+  font-family: 'Press Start 2P', cursive;
+  text-shadow: 1px 1px 5px #fff;
+`;
 
 const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Header>React Wars</Header>
       <CharacterList />
     </div>
   );
